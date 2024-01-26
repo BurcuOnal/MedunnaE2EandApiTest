977b651f91a2033daf0f62829a118f11c61d71dd
Burcu ÖNAL <burcuonal1145@gmail.com>
2024-01-26 15:08:07 +0300

    configReader Runner Driver add

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,46 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    private Driver(){
+
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if (driver==null){
+
+            switch (ConfigReader.getProperty("browser")) {
+
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        if (driver!=null){
+            driver.quit();
+        }
+        driver=null;
+    }
+}
---
+++ src/test/java/utilities/Runner.java
@@ -0,0 +1,29 @@
+package utilities;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty",
+        "html:target/default-cucumber-reports.html",
+        "json:target/json-reports/cucumber1.json",
+        "junit:target/xml-report/cucumber.xml",
+        "rerun:TestOutput/failed_scenario.txt"},
+        features = "src/test/resources",
+        glue = {"step_definitions","hooks"},
+        tags = "@e2e",
+        dryRun = false,
+        monochrome = true
+
+)
+
+
+
+
+
+
+public class Runner {
+
+}
